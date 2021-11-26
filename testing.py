3f1c7e102c2c2c2de0b40ad7dcbec71a91724426
Ryan Laurents <54565989+laurentsR@users.noreply.github.com>
2021-11-26 16:37:01 -0600

    Working summary
    Able to choose number of sentences to produce. Also pushed additional test files.

--- testing.py
+++ testing.py
@@ -12,6 +12,10 @@ import speech_recognition as sr
 # Possibly import more or less from nltk (TBD)
 # from nltk.corpus import stopwords
 # from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.corpus import stopwords
+from nltk.cluster.util import cosine_distance
+import numpy as np
+import networkx as nx
 
 def uploadAudio(self, choice):
     if(choice):
@@ -35,11 +39,11 @@ def get_large_audio_transcription(path):
     and apply speech recognition on each of these chunks
     """
     # open the audio file using pydub
-    sound = AudioSegment.from_wav(path)
+    sound = AudioSegment.from_file(path)
     # split audio sound where silence is 700 miliseconds or more and get chunks
     chunks = split_on_silence(sound,
         # experiment with this value for your target audio file
-        min_silence_len = 500,
+        min_silence_len = 700,
         # adjust this per requirement
         silence_thresh = sound.dBFS-14,
         # keep the silence for 1 second, adjustable as well
@@ -71,15 +75,91 @@ def get_large_audio_transcription(path):
     # return the text for all chunks detected
     return whole_text
 
+def sentence_similarity(sent1, sent2, stopwords=None):
+    if stopwords is None:
+        stopwords = []
+
+    sent1 = [w.lower() for w in sent1]
+    sent2 = [w.lower() for w in sent2]
+
+    all_words = list(set(sent1 + sent2))
+
+    vector1 = [0] * len(all_words)
+    vector2 = [0] * len(all_words)
+
+    # build the vector for the first sentence
+    for w in sent1:
+        if w in stopwords:
+            continue
+        vector1[all_words.index(w)] += 1
+
+    # build the vector for the second sentence
+    for w in sent2:
+        if w in stopwords:
+            continue
+        vector2[all_words.index(w)] += 1
+
+    return 1 - cosine_distance(vector1, vector2)
+
+def read_article(file_name):
+    file = open(file_name, "r")
+    filedata = file.readlines()
+    article = filedata[0].split(". ")
+    sentences = []
+
+    for sentence in article:
+        print(sentence)
+        sentences.append(sentence.replace("[^a-zA-Z]", " ").split(" "))
+    sentences.pop()
+
+    return sentences
+
+def build_similarity_matrix(sentences, stop_words):
+    # Create an empty similarity matrix
+    similarity_matrix = np.zeros((len(sentences), len(sentences)))
+
+    for idx1 in range(len(sentences)):
+        for idx2 in range(len(sentences)):
+            if idx1 == idx2: #ignore if both are same sentences
+                continue
+            similarity_matrix[idx1][idx2] = sentence_similarity(sentences[idx1], sentences[idx2], stop_words)
+
+    return similarity_matrix
+
+def generate_summary(text, top_n=5):
+    #nltk.download('stopwords')
+    stop_words = stopwords.words('english')
+    summarize_text = []
+
+    # Step 1 - Read text anc split it
+    sentences =  read_article(text)
+
+    # Step 2 - Generate Similary Martix across sentences
+    sentence_similarity_martix = build_similarity_matrix(sentences, stop_words)
+
+    # Step 3 - Rank sentences in similarity martix
+    sentence_similarity_graph = nx.from_numpy_array(sentence_similarity_martix)
+    scores = nx.pagerank(sentence_similarity_graph)
+
+    # Step 4 - Sort the rank and pick top sentences
+    ranked_sentence = sorted(((scores[i],s) for i,s in enumerate(sentences)), reverse=True)
+    print("Indexes of top ranked_sentence order are ", ranked_sentence)
+
+    for i in range(top_n):
+      summarize_text.append(" ".join(ranked_sentence[i][1]))
+
+    # Step 5 - Offcourse, output the summarize texr
+    print("Summarize Text: \n", ". ".join(summarize_text))
+
+
 
 print("In main")
-audioFile = "test_recording.m4a"
-path = "test_conversion.wav"
-#subprocess.call(['ffmpeg', '-i', 'test_recording.m4a', 'test_conversion.wav'])
-#wavFile = AudioSegment.from_file(audioFile).export("test_conversion.wav", format="wav")
-#print("After conversion")
-# generatedText = speechToText(wavFile)
-# print(generatedText)
+#path = "test_conversion2.wav"
+path = "test_recording2.m4a"
 text = get_large_audio_transcription(path)
 print(text)
-#textSummarizer(generatedText)
+text_file = open("temp.txt", "w")
+text_file.write(text)
+text_file.close()
+
+generate_summary("temp.txt", 2)
