195ed55605a79b5e73875583f7faf7c8360c164c
Ryan Laurents <54565989+laurentsR@users.noreply.github.com>
2021-12-02 20:38:56 -0600

    Cleaned up code

--- AI_Website/app.py
+++ AI_Website/app.py
@@ -1,3 +1,16 @@
+# Madison Adams and Ryan Laurents
+# CSE 4308 - Artificial Intelligence
+# Due 12/2/2021
+# Term Project - Meeting Summary Generator
+
+# Project Details:
+# This repository runs a web app which allows a user to record or upload audio or text
+# and the page will return a summary text file for the user to download. We also provide
+# the full transcription of audio files available for download. There are some custom 
+# variables that we allow the user to adjust: Silence Length and Number of Sentences in
+# the summary. The Silence length variable is the minimum length of silence we wait before
+# splitting them into separate sentences.
+
 from flask import Flask, render_template, request, flash, redirect, send_file
 import os
 import shutil
@@ -17,88 +30,81 @@ app.secret_key = 'super super secret key'
 # CHANGE THIS TO YOUR OWN BASE DIRECTORY
 baseDirectory = 'D:/Ryans School/CSE 4308 - Artificial Intelligence/Project/AI_Website/'
 
-def speechToText(audio):
-    r = sr.Recognizer()
-    with sr.AudioFile(audio) as source:
-        audio_data = r.record(source)
-        text = r.recognize_google(audio_data)
-    return text
-
+# This function splits a long audio file based on the user input silence length
+# Each split is considered a different sentence later on
 r = sr.Recognizer()
-def get_large_audio_transcription(path, silenceLength):
-    """
-    Splitting the large audio file into chunks
-    and apply speech recognition on each of these chunks
-    """
-    # open the audio file using pydub
+def transcribeLargeAudioFile(path, silenceLength):
+
     sound = AudioSegment.from_file(path)
-    # split audio sound where silence is 700 miliseconds or more and get chunks
+
+    # split audio sound where silence is silenceLength milliseconds or more and get the chunks
     chunks = split_on_silence(sound,
-        # experiment with this value for your target audio file
         min_silence_len = silenceLength,
         # adjust this per requirement
         silence_thresh = sound.dBFS-14,
-        # keep the silence for 1 second, adjustable as well
         keep_silence=500,
     )
-    folder_name = "audio-chunks"
+
+    folderName = "audio-chunks"
     # create a directory to store the audio chunks
-    if not os.path.isdir(folder_name):
-        os.mkdir(folder_name)
+    if not os.path.isdir(folderName):
+        os.mkdir(folderName)
     else:
-        shutil.rmtree(folder_name)
-        os.mkdir(folder_name)
-    whole_text = ""
-    # process each chunk
-    for i, audio_chunk in enumerate(chunks, start=1):
-        # export audio chunk and save it in
-        # the `folder_name` directory.
-        chunk_filename = os.path.join(folder_name, f"chunk{i}.wav")
-        audio_chunk.export(chunk_filename, format="wav")
-        # recognize the chunk
-        with sr.AudioFile(chunk_filename) as source:
-            audio_listened = r.record(source)
+        shutil.rmtree(folderName)
+        os.mkdir(folderName)
+    fullText = ""
+
+    # Process chunks
+    for i, audioChunk in enumerate(chunks, start=1):
+
+        chunkFileName = os.path.join(folderName, f"chunk{i}.wav")
+        audioChunk.export(chunkFileName, format="wav")
+
+        with sr.AudioFile(chunkFileName) as source:
+            audio = r.record(source)
             # try converting it to text
             try:
-                text = r.recognize_google(audio_listened)
+                text = r.recognize_google(audio)
             except sr.UnknownValueError as e:
                 print("Error:", str(e))
             else:
                 text = f"{text.capitalize()}. "
-                whole_text += text
-    # return the text for all chunks detected
-    return whole_text
+                fullText += text
+    
+    return fullText
 
-def sentence_similarity(sent1, sent2, stopwords=None):
+# Calculate the cosine similarity between sentences
+def cosineSimilarity(sent1, sent2, stopwords=None):
     if stopwords is None:
         stopwords = []
 
     sent1 = [w.lower() for w in sent1]
     sent2 = [w.lower() for w in sent2]
 
-    all_words = list(set(sent1 + sent2))
+    allWords = list(set(sent1 + sent2))
 
-    vector1 = [0] * len(all_words)
-    vector2 = [0] * len(all_words)
+    vector1 = [0] * len(allWords)
+    vector2 = [0] * len(allWords)
 
-    # build the vector for the first sentence
+    # Build the vector for the first sentence
     for w in sent1:
         if w in stopwords:
             continue
-        vector1[all_words.index(w)] += 1
+        vector1[allWords.index(w)] += 1
 
-    # build the vector for the second sentence
+    # Build the vector for the second sentence
     for w in sent2:
         if w in stopwords:
             continue
-        vector2[all_words.index(w)] += 1
+        vector2[allWords.index(w)] += 1
 
     return 1 - cosine_distance(vector1, vector2)
 
-def read_article(file_name):
+# Grab the sentences from the text file.
+def readText(file_name):
     file = open(file_name, "r")
-    filedata = file.readlines()
-    article = filedata[0].split(". ")
+    fileData = file.readlines()
+    article = fileData[0].split(". ")
     sentences = []
 
     for sentence in article:
@@ -108,47 +114,49 @@ def read_article(file_name):
 
     return sentences
 
-def build_similarity_matrix(sentences, stop_words):
+# Build the similarity matrix so we can return the top n
+def buildSimilarityMatrix(sentences, stopWords):
     # Create an empty similarity matrix
-    similarity_matrix = np.zeros((len(sentences), len(sentences)))
+    similarityMatrix = np.zeros((len(sentences), len(sentences)))
 
-    for idx1 in range(len(sentences)):
-        for idx2 in range(len(sentences)):
-            if idx1 == idx2: #ignore if both are same sentences
+    for i in range(len(sentences)):
+        for j in range(len(sentences)):
+            if i == j: #ignore if both are same sentences
                 continue
-            similarity_matrix[idx1][idx2] = sentence_similarity(sentences[idx1], sentences[idx2], stop_words)
+            similarityMatrix[i][j] = cosineSimilarity(sentences[i], sentences[j], stopWords)
 
-    return similarity_matrix
+    return similarityMatrix
+
 
 def generate_summary(text, top_n):
-    stop_words = stopwords.words('english')
-    summarize_text = []
+    stopWords = stopwords.words('english')
+    summary = []
 
-    # Step 1 - Read text anc split it
-    sentences =  read_article(text)
+    # Read text and split the sentences
+    sentences =  readText(text)
     numSentences = len(sentences)
 
-    # Step 2 - Generate Similary Martix across sentences
-    sentence_similarity_martix = build_similarity_matrix(sentences, stop_words)
+    # Generate the cosine similarity for all sentences
+    sentenceSimilarity = buildSimilarityMatrix(sentences, stopWords)
 
-    # Step 3 - Rank sentences in similarity martix
-    sentence_similarity_graph = nx.from_numpy_array(sentence_similarity_martix)
-    scores = nx.pagerank(sentence_similarity_graph)
+    # Rank sentences based on similarity score
+    similarityGraph = nx.from_numpy_array(sentenceSimilarity)
+    scores = nx.pagerank(similarityGraph)
 
-    # Step 4 - Sort the rank and pick top sentences
-    ranked_sentence = sorted(((scores[i],s) for i,s in enumerate(sentences)), reverse=True)
+    # Sort by rank and choose the top n sentences
+    orderedSentences = sorted(((scores[i],s) for i,s in enumerate(sentences)), reverse=True)
 
     requestedSentences = top_n
     if(numSentences < requestedSentences):
         requestedSentences = numSentences
 
     for i in range(requestedSentences):
-        summarize_text.append(" ".join(ranked_sentence[i][1]))
+        summary.append(" ".join(orderedSentences[i][1]))
 
 
-    # Step 5 - Offcourse, output the summarize text
+    # Write top n sentences to downloadable file
     f = open("summary.txt", "w")
-    f.write(". ".join(summarize_text))
+    f.write(". ".join(summary))
     f.close()
 
 @app.route("/")
@@ -165,7 +173,7 @@ def uploadFile():
         try:
             file = request.files['file']
             silenceLength = int(request.form['silenceLength'])
-            fullText = get_large_audio_transcription(file, silenceLength)
+            fullText = transcribeLargeAudioFile(file, silenceLength)
             textFile = open("fullText.txt", "w")
             textFile.write(fullText)
             textFile.close()
@@ -226,7 +234,7 @@ def summarizeFile():
             file = maxFile
 
             silenceLength = int(request.form['silenceLength'])
-            fullText = get_large_audio_transcription(file, silenceLength)
+            fullText = transcribeLargeAudioFile(file, silenceLength)
             textFile = open("fullText.txt", "w")
             textFile.write(fullText)
             textFile.close()
@@ -252,23 +260,11 @@ def textUpload():
 def uploadTextFile():
     if request.method == 'POST':
         try:
-            print("1")
             file = request.files['file']
-            print("2")
             file.save('temp.txt')
             n = int(request.form['n'])
-            # shutil.copyfile(file, 'fullText.txt')
-            # with open(file, 'r') as firstFile:
-            #     with open("fullText.txt", "a") as textFile:
-            #         for line in firstFile:
-            #             textFile.write(line)
-            # firstFile.close()
-            # textFile.close()
-            print("3")
             generate_summary("temp.txt", n)
-            print("4")
             summary = "summary.txt"
-            print("5")
             return render_template('textUpload.html', summary=summary)
         except:
             flash(f"Unexpected error when uploading file, please try again.")
